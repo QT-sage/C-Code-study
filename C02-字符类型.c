3cc3fab1a412dd2ef000158fb70a0d73902bd648
QT-sage <59397231+QT-sage@users.noreply.github.com>
2020-12-28 04:00:55 +0800

    Create C02-字符类型.c

---
+++ C02-字符类型.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    //内存
+    //char -- 字符类型
+    char ch = 'A';
+    //%c -- 打印字符格式的数据
+    printf("%c\n", ch);
+
+    int age = 20;
+    //%d -- 打印整型十进制数据
+    printf("%d\n", age);
+    return 0;
+}
+
+//char      字符数据类型
+//short     短整型
+//int       整型
+//long      长整型
+//long long 更长的整型
+//float     单精度浮点数
+//double    双精度浮点数
+
+//%d        打印整型
+//%c        打印字符
+//%f        打印浮点数
+//%p        以地址形式打印
+//%x        打印16进制
